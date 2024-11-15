068b0106dc90ddaf1fec5073a2fee93f515ab486
Denis Savishchev <denis.savishchev@gmail.com>
2024-11-15 17:09:15 +0100

    Initial Commit

---
+++ OnlineShop/OnlineShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopApp.swift
+//  OnlineShop
+//
+//  Created by Devis on 15/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
